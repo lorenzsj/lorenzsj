1a18e764da9be384b73b89505e978fde144dba29
lorenzsj <lorenzsj@clarkson.edu>
2020-05-11 22:22:47 +0000

    rewriting mockup to react app

---
+++ app/src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import NavigationBar from './NavigationBar';
+
+function App() {
+  return (
+    <div>
+      <NavigationBar />
+    </div>
+  );
+}
+
+export default App;
---
+++ app/src/components/NavigationBar.js
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
+
+function NavigationBar() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <Navbar collapseOnSelect expand="sm" bg="light" variant="light">
+      <Navbar.Brand href="#home">lorenzsj.io</Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#about">About</Nav.Link>
+          <Nav.Link href="#blog">Blog</Nav.Link>
+          <Nav.Link href="#portfolio">Portfolio</Nav.Link>
+          <Nav.Link href="#contact">Contact</Nav.Link>
+        </Nav>
+        <Nav>
+          {/* TODO: Move this into the LoginModal component */}
+          <Nav.Link href="#login">
+            <Button variant="primary" onClick={handleShow}>
+              Login
+            </Button>
+          </Nav.Link>
+          <Modal show={show} onHide={handleClose}>
+            <Modal.Header closeButton>
+              <Modal.Title>Login</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+            <Modal.Footer>
+              <Button variant="secondary" onClick={handleClose}>
+                Close
+              </Button>
+              <Button variant="primary" onClick={handleClose}>
+                Save Changes
+              </Button>
+            </Modal.Footer>
+          </Modal>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+}
+
+  export default NavigationBar;
