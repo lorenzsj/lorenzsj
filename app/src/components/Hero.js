22b509deecfc10eb69a12b19b63f4832ced172ad
lorenzsj <lorenzsj@clarkson.edu>
2020-05-13 03:50:15 +0000

    updated hero section to contain 2 columns

--- app/src/components/Hero.js
+++ app/src/components/Hero.js
@@ -5,18 +5,27 @@ import Jumbotron from 'react-bootstrap/Jumbotron';
 
 import './Hero.scss';
 
+import macbook from '../img/macbook.png';
+
 function Hero() {
   return (
-    <Jumbotron className="mb-0">
+    <Jumbotron fluid className="mb-0">
       <div className="container">
-        <h1>Hello, world!</h1>
-        <p>
-          This is a simple hero unit, a simple jumbotron-style component for calling
-          extra attention to featured content or information.
-        </p>
-        <p>
-          <Button variant="primary">Learn more</Button>
-        </p>
+      <div className="d-flex row align-items-center">
+          <div className="col-lg-6 order-2 order-lg-1">
+            <div>
+              <h1>Like What You See?</h1>
+              <p class="lead">Proident eu nisi commodo enim deserunt enim duis sunt nostrud anim. Sunt do sit enim veniam nostrud culpa adipisicing do ullamco occaecat et. </p>
+              <p className="mb-0">
+                <Button className="mr-2 shadow" variant="primary">Hire Me</Button>
+                <Button className="mr-2 shadow" variant="outline-primary">My Resume</Button>
+              </p>
+            </div>
+          </div>
+          <div className="col-lg-6 order-1 order-lg-2">
+            <img src={macbook} alt="..." class="img-fluid"/>
+          </div>
+        </div>
       </div>
     </Jumbotron>
   );
